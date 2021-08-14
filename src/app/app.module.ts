47e0ca88122d2b23c1c4a93741da110883f38aa0
asmaaMous556 <asmaamosmoh@gmail.com>
2021-08-14 14:06:06 +0200

    make-the-verfication-component-desing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { VerificationComponent } from './components/verification/verification.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    VerificationComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
